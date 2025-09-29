bc299bf0123adde15deadf11e67f4fe25059555b
ali <zakilakim717@gmail.com>
2025-09-29 11:12:38 +0100

    Tall-enough-to-ride-a-roller-coaster.py

---
+++ Tall-enough-to-ride-a-roller-coaster.py
@@ -0,0 +1,7 @@
+# ask the user for his or her tallness in cm
+tallnes = int(input("how tall are you in cm ? "))
+# see if the use above 145 cm tall
+if tallnes >= 145 :
+    print("YOU ARE Tall enough to ride a roller coaster")
+else :
+    print("sorry you are not Tall enough to ride a roller coaster")
--- what-is-your-name.py
+++
@@ -1,8 +0,0 @@
-#ask the user for his or her name 
-username = input("what is you name ?").lower()
-# check if our names are the same or not 
-if username == "ali" :
-    print("we have the same name hahahaha")
-
-else :
-    print(f'nice to meet you {username}')
