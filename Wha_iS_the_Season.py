c29245ec163166a6850785b07fe0516889d33803
ali <zakilakim717@gmail.com>
2025-09-29 13:24:08 +0100

    add the seasom programme

---
+++ Wha_iS_the_Season.py
@@ -0,0 +1,10 @@
+# ask the user for the month 
+month = int(input("choose a the mponth you want (pleas enter the number of the omnth (1-12))"))
+if month>=3 and month<=5 :
+    print ("you are in Spring")
+elif month >=6 and month <= 8 : 
+    print ("you are in Summer")
+elif month >=9 and month <= 11 : 
+    print ("you are in Autumn")
+elif month >=12 and month <= 2 :
+    print ("you are in Winter")
